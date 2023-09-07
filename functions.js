8cff103c877b975fbddc6f96b713dea047b4951a
faberh12 <hernandezfabian828@gmail.com>
2023-09-07 15:20:32 +0200

    commit stories modified

--- functions.js
+++ functions.js
@@ -25,3 +25,33 @@ const funcion_series = () => {
         })
 
 }
+
+const funcion_comics_stories = () => {
+    const url = "https://gateway.marvel.com/v1/public/comics?ts=1&apikey=28bfea47e9dfebb99eb345f7006521c4&hash=6e7bf39a11e12436c2dfedfda7ed397d"
+    let comic
+    let divisor_comic
+    let storie
+    const contenedor_series = document.getElementById("series") 
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            data.data.results.forEach(element => {
+                divisor_comic = document.createElement("div")
+                comic = document.createElement("a")
+                storie = document.createElement("p")
+                storie.textContent = "No Author"
+                if (parseInt(element.stories.available) >0)
+                    storie.textContent = element.stories.items[0].name
+                comic.textContent = element.title
+                comic.href = element.urls[0].url
+                contenedor_series.appendChild(divisor_comic)
+                divisor_comic.appendChild(comic)
+                divisor_comic.appendChild(storie)
+
+            });
+            console.log("Status:200")
+            
+        })
+        .catch((error) => console.log("Status:100"))
+
+}
